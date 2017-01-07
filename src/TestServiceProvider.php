d28184d403955a571035690ba898ea97388fbaa6
Bindesh Pandya <bindeshpandya.iipl@gmail.com>
2017-01-07 12:01:50 +0000

    Upload new file

---
+++ src/TestServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace DemoPackage\Bleh;
+
+use Illuminate\Support\ServiceProvider;
+
+class TestServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    protected $commands = [
+        'DemoPackage\Bleh\Commands\MyCommand',
+
+    ];
+
+    public function boot()
+    {
+       require __DIR__.'/routes.php';
+
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+        $this->commands($this->commands);
+
+        $this->app['test'] = $this->app->share(function($app) {
+            return new Bleh;
+        });
+    }
+}
